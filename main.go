6ae5c8924903b4af634c2d36431f68d7143e3253
msledzikowski <mateuszsledzikowski1@gmail.com>
2023-10-17 13:29:07 +0200

    chore: establish mongodb connectivity

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	mongodb "main.go/mongoDB"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Some error occured. Err: %s \n", err)
+	}
+
+	m, err := mongodb.InitDB()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(m)
+}
