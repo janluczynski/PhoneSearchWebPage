a47d6a2b8a911926f7e5e2511d999d7c8d7f37c9
Bla1s <blazej1gg@gmail.com>
2023-10-30 16:35:35 +0100

    - now it adds items one by one to the db

--- scriptForProducts/scriptForProducts.go
+++ scriptForProducts/scriptForProducts.go
@@ -27,29 +27,29 @@ func main() {
 	m.DeleteAllProducts()
 
 	// Call the function to get database items from JSON file
-	products, err := getDatabaseItems("../Dataset/data.json")
+	err = getDatabaseItems(m, "../Dataset/data.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Insert products into MongoDB collection
-	err = m.AddProducts(products)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // Insert products into MongoDB collection
+	// err = m.AddProducts(products)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
-func getDatabaseItems(filePath string) ([]interface{}, error) {
+func getDatabaseItems(m *mongodb.MongoDB, filePath string) error {
 	// Read the JSON file into a byte slice
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var products []commons.Product
 	err = json.Unmarshal(data, &products)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	u := launcher.New().Bin("./bin/chrome").MustLaunch()
@@ -59,29 +59,13 @@ func getDatabaseItems(filePath string) ([]interface{}, error) {
 
 	// Add UUID to each product
 	for i := range products {
+		var product interface{}
 		products[i].ProductID = uuid.New().String()
 		//TODO: add scrapping images function to scram images from the web and add them to a []string and apped to the products
 		products[i].Imagetab = scrapper.Scrap(browser, products[i].ProductURL)
+		product = products[i]
+		m.AddProduct(product)
 	}
 
-	// Convert products to []interface{}
-	var interfaceProducts []interface{}
-	for _, p := range products {
-		interfaceProducts = append(interfaceProducts, p)
-	}
-
-	return interfaceProducts, nil
-
+	return nil
 }
-
-// func printProduct(product commons.Product) {
-// 	fmt.Println("Product ID:", product.ProductID)
-// 	fmt.Println("Product Name:", product.ProductName)
-// 	fmt.Println("Brand:", product.Brand)
-// 	fmt.Println("Image URL:", product.ImageURL)
-// 	fmt.Println("Sale Price:", product.SalePrice)
-// 	fmt.Println("Colour:", product.Colour)
-// 	fmt.Println("Description:", product.Description)
-// 	fmt.Println("Category:", product.Category)
-// 	fmt.Println("Material:", product.Material)
-// }
