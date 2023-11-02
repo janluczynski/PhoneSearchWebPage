0aff8dc1acdaaac6a4999106874edf718ab3cfb4
Bla1s <blazej1gg@gmail.com>
2023-11-02 20:16:18 +0100

    - added SearchProductsFromSearchBar in routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	mongodb "main.go/mongoDB"
 )
 
@@ -31,5 +35,46 @@ func PostProductInfo(r *gin.Engine, m *mongodb.MongoDB) {
 
 		// Sending data as JSON response
 		c.JSON(http.StatusOK, productData)
+	})
+}
+func SearchProductsFromSearchBar(r *gin.Engine, m *mongodb.MongoDB) {
+	r.POST("/search", func(c *gin.Context) {
+		searchedPhrase := c.PostForm("searchedPhrase")
+		if searchedPhrase == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "No variable in cookies files"})
+
+			return
+		}
+		filter := bson.M{
+			"$or": []bson.M{
+				{"product_name": bson.M{"$regex": primitive.Regex{Pattern: searchedPhrase, Options: ""}}},
+			},
+		}
+
+		var products []string
+		cursor, err := m.DatabaseCollection.Find(context.Background(), filter)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		defer cursor.Close(context.Background())
+
+		for cursor.Next(context.Background()) {
+			var result bson.M
+			err := cursor.Decode(&result)
+			if err != nil {
+				log.Printf("Error decoding document: %v\n", err)
+				continue
+			}
+
+			// Extract product_name field from the document and append it to the products slice
+			productName, ok := result["product_name"].(string)
+			if ok {
+				products = append(products, productName)
+			}
+		}
+
+		// Sending data as JSON response
+		c.JSON(http.StatusOK, products)
 	})
 }
