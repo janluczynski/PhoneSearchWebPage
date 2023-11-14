d24f539ff5e8b9bf0be3fe9aa67fb6662b40afc6
msledzikowski <mateuszsledzikowski1@gmail.com>
2023-11-14 22:16:33 +0100

    chore: make restructurization

---
+++ backend/Scraping/main.go
@@ -0,0 +1,12 @@
+package main
+
+import scrappers "main.go/scrapper"
+
+func main() {
+	//scrappers.XkomScrap()
+	// scrappers.XkomScrapProductInfo()
+	scrappers.FakeMediaMarktRequest()
+	// scrappers.MediaMarktScrap()
+	scrappers.MediaMarktScrapProductInfo()
+	//scrappers.Test2()
+}
