e8437dcd2b4b68a4ddf5db0a858ac7aa8fb83387
janluczynski <j.luczynski.450@studms.ug.edu.pl>
2023-11-20 20:22:51 +0100

    struktura react

---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import "./Navbar.css";
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+  } from '@chakra-ui/react'
+
+const Navbar = () => {
+
+    return(
+        <div class="navbar">
+        <Menu>
+  <MenuButton
+    as={IconButton}
+    aria-label='Options'
+    icon={<HamburgerIcon color="black" height="3vh" width="3vh"/>}
+    variant='outline'
+    height="4vh"
+    width="4vh"
+    borderRadius='lg'
+  />
+  <MenuList color="black">
+    <MenuItem height="3.5vh">
+      New Tab
+    </MenuItem>
+    <MenuItem height="3.5vh">
+      New Window
+    </MenuItem>
+    <MenuItem height="3.5vh">
+      Open Closed Tab
+    </MenuItem>
+    <MenuItem height="3.5vh">
+      Open File...
+    </MenuItem>
+  </MenuList>
+</Menu>
+        	</div> 
+    )
+}
+
+export default Navbar;
